b31182d4e783d03f7aa05f2048d0cbead0bd8423
TheRex <83464936+Trex2007@users.noreply.github.com>
2024-09-05 13:51:43 +0200

    Add files via upload

---
+++ WPF/ACT0/act0_AdrienBrahy_Ex1/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace act0_AdrienBrahy_Ex1
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
