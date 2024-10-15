36f9a549deb80905ee1ed58f753b436ed35a1c8c
TheRex <83464936+Trex2007@users.noreply.github.com>
2024-10-15 11:26:47 +0200

    inerro 2 poo

---
+++ POO/I2P624_BrahyAdrien/I2P624_BrahyAdrien/FeuDeSignalisation.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace I2P624_BrahyAdrien
+{
+    internal class FeuDeSignalisation
+    {
+        int _couleur;
+        string _identifiant;
+        bool _etat;
+
+        public FeuDeSignalisation(int Couleur, string Identifiant, bool Etat)
+        {
+            _couleur = Couleur;
+            _identifiant = Identifiant;
+            _etat = Etat;
+        }
+        public int Couleur
+        {
+            get { return _couleur; }
+            set { _couleur = value; }
+        }
+        public string Identifiant
+        {
+            get { return _identifiant; }
+            set { _identifiant = value; }
+        }
+        public bool Etat
+        {
+            get { return _etat; }
+            set { _etat = value; }
+        }
+        /// <summary>
+        /// afficher sa couleur en texte au lieu de int
+        /// </summary>
+        /// <returns></returns>
+        public string AfficheCouleur()
+        {
+            string laCouleur;
+
+            if (_couleur == 0)
+            {
+                laCouleur = "Vert";
+            } else if (_couleur == 1)
+            {
+                laCouleur = "Orange";
+            } else
+            {
+                laCouleur = "Rouge";
+            }
+
+            return laCouleur;
+        }
+        /// <summary>
+        /// afficher son etat en texte au lieu de bool
+        /// </summary>
+        /// <returns></returns>
+        public string AfficheEtat()
+        {
+            string leEtat = "Introuvable";
+
+            if(_etat == true)
+            {
+                leEtat = "Allumé";
+            } else
+            {
+                leEtat = "Eteint";
+            }
+
+            return leEtat;
+        }
+        /// <summary>
+        /// Passer a la couleur suivante
+        /// </summary>
+        /// <returns></returns>
+        public int ChangeCouleur()
+        {
+            int couleur = _couleur;
+            if (couleur > 1)
+            { 
+                couleur = 0; 
+            } 
+            else 
+            { 
+                couleur += 1; 
+            }
+            return couleur;
+        }
+        /// <summary>
+        /// allumer eteindre la lampe
+        /// </summary>
+        /// <param name="nbAlternance">nombre de fois qu'il le change</param>
+        public void Clignote(int nbAlternance)
+        {
+            Console.WriteLine("Le feu de signalisations " + Identifiant + " était au départ " + AfficheEtat());
+            for (int i = 0; i < nbAlternance; i++)
+            {
+                _etat = !_etat;
+                Console.WriteLine("Le feu de signalisations "+Identifiant+ " est maintenant " + AfficheEtat());
+            }
+        }
+        public void ChangeEtat()
+        {
+            _couleur = ChangeCouleur();
+        }
+    }
+}
---
+++ POO/I2P624_BrahyAdrien/I2P624_BrahyAdrien/Program.cs
@@ -0,0 +1,61 @@
+﻿namespace I2P624_BrahyAdrien
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            FeuDeSignalisation FeuCoin = new FeuDeSignalisation(0, "coinDeRue", false);
+            FeuDeSignalisation FeuBat = new FeuDeSignalisation(0, "BatDeRue", false);
+            Console.WriteLine("A = Infos | B = Changer Etat | C = Clignotant | D = ChangeCouleur");
+            int nbAlternance = 5;
+            while (true)
+            {
+                
+                switch (Console.ReadKey(true).Key)
+                {
+                    case ConsoleKey.A:
+                        Console.Clear();
+                        Console.WriteLine("A = Infos | B = Changer Etat | C = Clignotant | D = ChangeCouleur\n");
+
+                        Console.WriteLine("Etats et Couleurs des feux :\n--------------------------");
+
+                        Console.WriteLine("Le feu de Signalisation " + FeuCoin.Identifiant + " est " + FeuCoin.AfficheCouleur() + " et est " + FeuCoin.AfficheEtat());
+                        Console.WriteLine("Le feu de Signalisation " + FeuBat.Identifiant + " est " + FeuBat.AfficheCouleur() + " et est " + FeuBat.AfficheEtat());
+                        break;
+                    case ConsoleKey.B:
+                        Console.Clear();
+                        Console.WriteLine("A = Infos | B = Changer Etat | C = Clignotant | D = ChangeCouleur\n");
+
+                        Console.WriteLine("Faire passer " + FeuCoin.Identifiant+ " a l'orange\n--------------------------");
+                        FeuCoin.Couleur = 1;
+                        FeuCoin.Etat = true;
+                        Console.WriteLine("Le feu de Signalisation " + FeuCoin.Identifiant + " est " + FeuCoin.AfficheCouleur() + " et est " + FeuCoin.AfficheEtat());
+                        break;
+                    case ConsoleKey.C:
+                        Console.Clear();
+                        Console.WriteLine("A = Infos | B = Changer Etat | C = Clignotant | D = ChangeCouleur\n");
+
+                        Console.WriteLine("Feu Clignotant\n--------------------------");
+                        FeuCoin.Clignote(nbAlternance);
+
+                        break;
+                    case ConsoleKey.D:
+                        Console.Clear();
+                        Console.WriteLine("A = Infos | B = Changer Etat | C = Clignotant | D = ChangeCouleur\n");
+                        
+                        Console.WriteLine("Changement d'état\n--------------------------");
+
+                        Console.WriteLine("Le feu de signalisation" + FeuBat.Identifiant + " était au départ " + FeuBat.AfficheCouleur());
+                        for (int i = 0; i < 4; i++)
+                        {
+                            FeuBat.ChangeEtat();
+                            Console.WriteLine("Le feu de signalisation"+ FeuBat.Identifiant +" est maintenant " + FeuBat.AfficheCouleur());
+                        }
+
+                        break;
+
+                }
+            }
+        }
+    }
+}
