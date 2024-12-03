b3e4e9258ad6c6555b7bee5f62c7208a9f4007ca
TheRex <83464936+Trex2007@users.noreply.github.com>
2024-12-03 11:48:02 +0100

    act7

---
+++ POO/ACT_7/ACT-7_AdrienB_CRUDetBDD/Program.cs
@@ -0,0 +1,69 @@
+﻿using Google.Protobuf.WellKnownTypes;
+using System.Data;
+
+namespace ACT_7_AdrienB_CRUDetBDD
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            method methodes = new method(); // isntanciation d ela structure
+
+            DataSet infos;
+
+            while (true)
+            {
+                Console.WriteLine("\n [A] = All User | [B] All Biens");
+                switch (Console.ReadKey(true).Key)
+                {
+                    case ConsoleKey.A:
+                        Console.Clear();
+
+                        if (methodes.allUser(out infos))
+                        {
+                            for (int i = 0; i < infos.Tables[0].Rows.Count; i++)
+                            {
+                                Console.Write(infos.Tables[0].Rows[i]["id"] + ", ");
+                                Console.Write(infos.Tables[0].Rows[i]["nomUser"] + ", ");
+                                Console.Write(infos.Tables[0].Rows[i]["prenomUser"] + ", ");
+                                Console.Write(infos.Tables[0].Rows[i]["loginUser"] + ", ");
+                                Console.Write(infos.Tables[0].Rows[i]["passWordUser"] + ", ");
+                                Console.Write(infos.Tables[0].Rows[i]["role"] + "\n\n");
+                            }
+                        }
+                        
+                        break;
+                    case ConsoleKey.B:
+                        Console.Clear();
+
+                        if (methodes.allBiens(out infos))
+                        {
+                            for (int i = 0; i < infos.Tables[0].Rows.Count; i++)
+                            {
+                                Console.Write("\n\n" + infos.Tables[0].Rows[i]["bienid"] + ", ");
+                                Console.Write(infos.Tables[0].Rows[i]["nom"] + ", ");
+                                Console.Write(infos.Tables[0].Rows[i]["taille"] + ", ");
+                                Console.Write(infos.Tables[0].Rows[i]["prix"] + ", ");
+                                Console.Write(infos.Tables[0].Rows[i]["ville"] + ", ");
+                                Console.Write(infos.Tables[0].Rows[i]["userId"] + ", ");
+                                Console.Write(infos.Tables[0].Rows[i]["description"] + ", ");
+                                Console.Write(infos.Tables[0].Rows[i]["chambres"] + ", ");
+                                Console.Write(infos.Tables[0].Rows[i]["imageBien"] + "\n");
+                                int idDuBien = (int)infos.Tables[0].Rows[i]["bienid"];
+                                if (methodes.allOptionBiens(idDuBien, out DataSet options))
+                                {
+                                    for (int y = 0; y < options.Tables[0].Rows.Count; y++)
+                                    {
+                                        Console.Write(options.Tables[0].Rows[y]["optionNom"] + ", ");
+                                    }
+                                }
+                            }
+                        }
+
+                        break;
+                }
+                
+            }
+        }
+    }
+}
---
+++ POO/ACT_7/ACT-7_AdrienB_CRUDetBDD/method.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using MySql.Data.MySqlClient;
+using System.Diagnostics;
+using Google.Protobuf.WellKnownTypes;
+using System.Reflection.Metadata.Ecma335;
+using System.Data.Common;
+using System.Net;
+using System.Runtime.ConstrainedExecution;
+
+namespace ACT_7_AdrienB_CRUDetBDD
+{
+    public struct method
+    {
+        static string DefinirCheminBD() // détermine la chaîne de connexion
+        {
+            return "server=localhost;database=poo-test;port=3306;User Id=root;password=root";
+        }
+        public bool allUser(out DataSet infos)
+        {
+            bool ok = false;
+            try
+            {
+                MySqlConnection allUsers = new MySqlConnection(DefinirCheminBD());
+                infos = new DataSet();
+
+                allUsers.Open();
+
+                string query = "SELECT * FROM utilisateurs;";
+
+                MySqlDataAdapter da = new MySqlDataAdapter(query, allUsers);
+
+                da.Fill(infos, "mesDonnees");
+
+
+                allUsers.Close();
+
+                if(infos.Tables[0].Rows.Count > 0)
+                {
+                    ok = true;
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+                throw;
+            }
+            return ok;
+        }
+        public bool allBiens(out DataSet infos)
+        {
+            bool ok = false;
+            try
+            {
+                MySqlConnection allUsers = new MySqlConnection(DefinirCheminBD());
+                infos = new DataSet();
+
+                allUsers.Open();
+
+                string query = "SELECT * FROM biens;";
+
+                MySqlDataAdapter da = new MySqlDataAdapter(query, allUsers);
+
+                da.Fill(infos, "mesDonnees");
+
+
+                allUsers.Close();
+
+                if (infos.Tables[0].Rows.Count > 0)
+                {
+                    ok = true;
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+                throw;
+            }
+            return ok;
+        }
+        public bool allOptionBiens(int id,out DataSet infos)
+        {
+            bool ok = false;
+            try
+            {
+                MySqlConnection allUsers = new MySqlConnection(DefinirCheminBD());
+                infos = new DataSet();
+
+                allUsers.Open();
+
+                string query = "SELECT * FROM options WHERE optionId IN(Select optionId from biens_options Where bienId = "+id+");" ;
+
+                MySqlDataAdapter da = new MySqlDataAdapter(query, allUsers);
+
+                da.Fill(infos, "mesDonnees");
+
+
+                allUsers.Close();
+
+                if (infos.Tables[0].Rows.Count > 0)
+                {
+                    ok = true;
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+                throw;
+            }
+            return ok;
+        }
+    }
+}
