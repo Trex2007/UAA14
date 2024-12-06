58f9fc06e4869103b848e3a9ee350dd5b5079f7e
TheRex <83464936+Trex2007@users.noreply.github.com>
2024-12-06 22:30:06 +0100

    Add files via upload

---
+++ POO/ACT_6_Heritage_2/ACT6_HERITAGE_2/Program.cs
@@ -0,0 +1,16 @@
+﻿using ACT6_HERITAGE_2.Class;
+
+namespace ACT6_HERITAGE_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectangle monRectangle = new Rectangle("Rouge", new float[] { 5, 10 });
+            Console.WriteLine(monRectangle.CalculerSurface());
+
+            Carre monCarre = new Carre("Bleu", 6);
+            Console.WriteLine(monCarre.CalculerSurface());
+        }
+    }
+}
